1d457a23d687c44b8208c8a2fdcdcd6bc67a47ed
albayrrak <92863497+albayrrak@users.noreply.github.com>
2023-07-16 23:39:29 +0300

    env cleared

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
+require('dotenv').config();
 
 app.get('/', (req, res) => {
-  res.send('hello2');
+  res.send('hello2n');
 });
 
-app.listen('5000', () => {
+app.listen(process.env.PORT, () => {
   console.log('server is running');
 });
